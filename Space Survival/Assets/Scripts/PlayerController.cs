7dd476a72c030f1ad411e2146bd34fce4b300bde
Alexandru Vasilescu <alexandru.star@gmail.com>
2016-04-02 23:08:27 +0300

    First commit
    Project files added

---
+++ Space Survival/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	void Start () {
+	    
+	}
+	
+	void Update () {
+	    //transform.Translate(transform.position)
+	}
+}
